a391b047d11573163ab9789bb98608b54d608618
dongchengqiao <wojiaodongchengqiao@outlook.com>
2025-11-09 23:03:04 +0800

    添加基础

---
+++ src/main/java/com/dongchengqiao/example/ExampleModClient.java
@@ -0,0 +1,10 @@
+package com.dongchengqiao.example;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ExampleModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
