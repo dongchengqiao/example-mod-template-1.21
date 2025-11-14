76f8a3f3a3f3c752eb406ba899fb82029c34ae2d
dongchengqiao <wojiaodongchengqiao@outlook.com>
2025-11-14 12:36:47 +0800

    添加item

--- src/main/java/com/dongchengqiao/example/ExampleMod.java
+++ src/main/java/com/dongchengqiao/example/ExampleMod.java
@@ -1,5 +1,6 @@
 package com.dongchengqiao.example;
 
+import com.dongchengqiao.example.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -19,6 +20,7 @@ public class ExampleMod implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
+		ModItems.registerItems();
 		LOGGER.info("Hello Fabric world!");
 	}
 }
---
+++ src/main/java/com/dongchengqiao/example/item/ModItems.java
@@ -0,0 +1,24 @@
+package com.dongchengqiao.example.item;
+
+import com.dongchengqiao.example.ExampleMod;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+
+    //第一个物品
+    public static final Item New_Item = registerItem("new_item", new Item(new Item.Settings()));
+
+    private static Item registerItem(String  id, Item item){
+        return Registry.register(Registries.ITEM, Identifier.of(ExampleMod.MOD_ID,id), item);
+//      return Registry.register(Registries.ITEM, RegistryKey.of(Registries.ITEM.getKey(), Identifier.of(ExampleMod.MOD_ID,id)),item);
+    }
+
+    //加载
+    public static void registerItems() {
+        ExampleMod.LOGGER.info("Registering Items");
+    }
+}
