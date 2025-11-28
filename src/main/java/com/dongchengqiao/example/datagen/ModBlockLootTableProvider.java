6c23b7f0195cb1b354a47da138baf44fe63be8e3
dongchengqiao <wojiaodongchengqiao@outlook.com>
2025-11-28 22:28:24 +0800

    更新gradle

--- src/main/java/com/dongchengqiao/example/datagen/ModBlockLootTableProvider.java
+++ src/main/java/com/dongchengqiao/example/datagen/ModBlockLootTableProvider.java
@@ -1,4 +1,19 @@
 package com.dongchengqiao.example.datagen;
 
-public class ModBlockLootTableProvider {
+import com.dongchengqiao.example.block.ModBlocks;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricBlockLootTableProvider;
+import net.minecraft.registry.RegistryWrapper;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ModBlockLootTableProvider extends FabricBlockLootTableProvider {
+    protected ModBlockLootTableProvider(FabricDataOutput dataOutput, CompletableFuture<RegistryWrapper.WrapperLookup> registryLookup) {
+        super(dataOutput, registryLookup);
+    }
+
+    @Override
+    public void generate() {
+
+    }
 }
